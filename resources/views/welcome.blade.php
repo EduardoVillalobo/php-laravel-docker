b86f695ba599b67a17ec3bffcc60285e8a26dcd3
Eduardo Villalobo <eduardovillalobo23@gmail.com>
2022-11-15 08:29:32 -0300

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">CASA</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
